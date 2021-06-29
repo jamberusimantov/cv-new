64c74bdb4c47945f66cec3deb498e6eae3625589
jamberusimantov <jamberu.simantov@gmail.com>
2021-06-29 22:43:07 +0300

    .

--- index.js
+++ index.js
@@ -3,31 +3,22 @@ window.onload = () => {
     hamburger.addEventListener('click', menuToggle)
 
     const intro = document.getElementById('intro')
-    const sentences = document.getElementsByClassName('about-text-sentence')
     const sections = document.getElementsByClassName('section')
     const span = 'Coding is fun and at the end of the day, if I can say I had fun It was a good day.';
-    const sentence = [
-        'My name is Siman-tov, and I’m a Full Stack web developer.',
-        'I’m self-motivated and enjoy expanding my knowledge.',
-        'Recently I completed a 2000 hours of theoretic and practical studies in programming including HTML, CSS, Javascript, React, Redux, TypeScript, Nodejs, API Using Express, MongoDB, and... OOP, among others.',
-        'I read a lot and consult professionals so I’m sure there’s no challenge I can’t face.',
-        'I’m currently looking for a Full Stack position, but I’m open to other positions in this area for acquiring experience and broadening my knowledge.'
-    ];
+
     const introWordsArray = span.split(" ");
     const sectionsArray = new Array()
     const interval = 50;
-    let sentencesCounter = 0;
     let introCounter = 0;
-    let aboutTextCounter = 0;
     let offsetCounter = 0;
     let sectionsIndex = 0;
     let sliderIndex = 0;
     let isFirstScroll = true;
     let isMenuOpen = false;
-    let skillsCounter = 0;
     const myProjects = [
         ['users-portal', './assets/myProjects/mern- users.jpg', ''],
         ['jobOffers portal', './assets/myProjects/jobPortal_TechCareer.jpg', ''],
+        ['memories', './assets/myProjects/memories.jpg', 'https://memories-my-app.herokuapp.com/'],
     ]
     const sliderPicsArray = [];
     for (const key in sections) {
@@ -85,10 +76,10 @@ window.onload = () => {
         flipCardFront.lastElementChild.classList.add('flip_img')
         flipCardFront.lastElementChild.src = project[1];
         const flipCardBack = document.getElementsByClassName('flip-card-back')[document.getElementsByClassName('flip-card-back').length - 1]
-        flipCardBack.appendChild(document.createElement('h1'))
-        flipCardBack.lastElementChild.appendChild(document.createTextNode(project[0]))
-        flipCardBack.appendChild(document.createElement('p'))
-        flipCardBack.lastElementChild.appendChild(document.createTextNode(project[2]))
+        flipCardBack.appendChild(document.createElement('a'))
+        flipCardBack.lastElementChild.href = project[2];
+        flipCardBack.lastElementChild.alt = project[1];
+        flipCardBack.lastElementChild.innerHTML = project[0];
     }
 
     function scrollHandler() {
@@ -118,7 +109,7 @@ window.onload = () => {
             default:
                 {
                     if (isFirstScroll) {
-                        pushText(sentence[sentencesCounter].split(" "), interval, sentences[sentencesCounter], aboutTextCounter, "summery-word")
+                        // pushText(sentence[sentencesCounter].split(" "), interval, sentences[sentencesCounter], aboutTextCounter, "summery-word")
                         setInterval(changePic, 2000);
                         isFirstScroll = false;
                     }
@@ -138,84 +129,28 @@ window.onload = () => {
             day: 'day',
             good: 'good'
         }
-        const skills = {
-            html: 'HTML',
-            css: 'CSS',
-            javascript: 'Javascript',
-            react: 'React',
-            redux: 'Redux',
-            typescript: 'TypeScript',
-            nodejs: 'Nodejs',
-            api: 'API',
-            using: 'Using',
-            express: 'Express',
-            mongodb: 'MongoDB',
-        };
-        if (!arr.length && node.id === `sentence${sentencesCounter}` && sentencesCounter < sentences.length - 1) {
-            setTimeout(() => {
-                pushText(sentence[++sentencesCounter].split(" "), interval, sentences[sentencesCounter], aboutTextCounter, "summery-word")
-            }, 500)
-        }
+
         if (!arr.length) return;
         const word = arr.shift()
         const isBuzzWord = buzzWords[word.toLowerCase()]
-        const isSkill = skills[word.toLowerCase()] || skills[word.toLowerCase().substr(0, word.length - 1)]
         if (isBuzzWord) {
             node.innerHTML += `<em class="${className}"></em>`
-        } else if (isSkill) {
-            if (!document.getElementById('skills')) {
-                node.innerHTML += `<em id="skills"></em>`
-            }
         } else {
             node.innerHTML += `<span class="${className}"></span>`
         }
-        if (isSkill) {
-            const sameElement = document.getElementById('skills')
-            const pushTextInterval = setInterval(() => {
-                let charToPush = word.charAt(counter++);
-                if (!charToPush) {
-                    sameElement.textContent = sameElement.textContent.trim() === skills.mongodb ||
-                        sameElement.textContent.substr(0, sameElement.textContent.length - 1).trim() === skills.mongodb ?
-                        `${sameElement.textContent} ` : ' ';
-                    clearInterval(pushTextInterval);
-                    counter = 0
-                        // setTimeout(() => {
-                    pushText(arr, 100, node, counter, className)
-                        // }, 500)
-                } else {
-                    sameElement.textContent += charToPush;
-                }
-            }, interval)
 
-        } else {
-            // setInterval(() => {
-            //     const { html, css, javascript, react, redux, typescript, nodejs, api, using, express, mongodb } = skills
-            //     const skillsArray = [html, css, javascript, react, redux, typescript, nodejs, api, using, express, mongodb]
-            //     sameElement.textContent = skillsCounter < skillsArray.length - 1 ? skillsArray[skillsCounter++] : skillsArray[0]
-            //     console.log(skillsCounter);
-            // }, 1000);
-            const newElement = document.getElementsByClassName(className)[document.getElementsByClassName(className).length - 1]
-            const pushTextInterval = word === '2000' ? setInterval(() => {
-                if (counter !== 200) {
-                    newElement.textContent = ++counter * 10;
-                } else {
-                    clearInterval(pushTextInterval);
-                    counter = 0
-                    newElement.textContent += ' ';
-                    pushText(arr, interval, node, counter, className)
-                }
+        const newElement = document.getElementsByClassName(className)[document.getElementsByClassName(className).length - 1]
+        const pushTextInterval = setInterval(() => {
+            let charToPush = word.charAt(counter++);
+            if (!charToPush) {
+                clearInterval(pushTextInterval);
+                counter = 0
+                newElement.textContent += ' ';
+                pushText(arr, interval, node, counter, className)
+            }
+            newElement.textContent += charToPush;
+        }, interval)
 
-            }, 0) : setInterval(() => {
-                let charToPush = word.charAt(counter++);
-                if (!charToPush) {
-                    clearInterval(pushTextInterval);
-                    counter = 0
-                    newElement.textContent += ' ';
-                    pushText(arr, interval, node, counter, className)
-                }
-                newElement.textContent += charToPush;
-            }, interval)
-        }
     }
 
     function changePic() {
